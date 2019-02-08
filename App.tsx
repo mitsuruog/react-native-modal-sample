f6ed8a670eafcebfead29c591452327d7740f58c
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2019-02-08 16:08:12 +0900

    Add react-native-modal

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ export default class App extends React.Component<{}, {}> {
   render() {
     return (
       <View style={styles.container}>
-        <Text>dead-simple-expo-typescript-starter</Text>
+        <Text>react-native-modal-sample</Text>
       </View>
     );
   }
